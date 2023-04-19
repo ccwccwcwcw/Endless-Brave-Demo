793861e8498f9ab0d9ecff153b066447a7567b47
unknown <a1@tyit35643.vtc.hk>
2023-04-19 13:56:40 +0800

    ai3

--- Assets/sword.cs
+++ Assets/sword.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class sword : MonoBehaviour
 {
     public int damageAmount = 10;
-    private void Start£¨£©{
+    private void Start() {
         Destroy(gameObject,10);
     }
     private void OnTriggerEnter(Collider other)
     {
-    Destroy(transform.GetComponent<Rigibody>());
+    Destroy(transform.GetComponent<Rigidbody>());
     if (other.tag == "Dragon") {
         other.GetComponent<Dragon>().TakeDamage(damageAmount);
     }
