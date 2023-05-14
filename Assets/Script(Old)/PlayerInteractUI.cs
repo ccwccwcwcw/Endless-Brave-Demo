1aaf21022125881c79454b715805f4434ec94c76
carfk <louis200001@gmail.com>
2023-05-14 23:34:23 +0800

    Update  PlayerInteractUI

--- Assets/Script(Old)/PlayerInteractUI.cs
+++ Assets/Script(Old)/PlayerInteractUI.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class PlayerInteractUI : MonoBehaviour
 {
     [SerializeField] private GameObject containerGameObject;
+    [SerializeField] private PlayerInteract playerInteract;
+
+    private void Update() { 
+        if(playerInteract.GetInteractableObject() != null)
+        {
+            Show();
+            else
+            {
+                Hide();
+            }
+        }
+    }
     private void Show()
     {
         containerGameObject.SetActive(true);
