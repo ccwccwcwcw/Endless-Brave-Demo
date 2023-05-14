45a5f526aa94e8f824301d5e6c5a0afc419cd0b9
EasyTroll <easytroll167@gmail.com>
2023-05-15 03:10:32 +0800

    dragon sound and health bar

--- Assets/Dragon.cs
+++ Assets/Dragon.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dragon : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int HP = 100;
+    private int HP = 100;
+    public Slider healthBar;
     public Animator animator;
 
     private void OnTriggerEnter(Collider other)
     {
+        
+        
         if (other.tag == "OHSword")
         {
             TakeDamage(10);
@@ -30,7 +34,11 @@ public class Dragon : MonoBehaviour
             TakeDamage(14);
             Debug.Log("collision");
         }
-       
+  
+    }
+    void Update()
+    {
+        healthBar.value = HP;
     }
 
     public void TakeDamage(int damageAmount)
@@ -38,12 +46,14 @@ public class Dragon : MonoBehaviour
         HP -= damageAmount;
         if (HP <= 0)
         {
+            AudioManager.instance.Play("DragonDeath");
             animator.SetTrigger("die");            
             GetComponent<Collider>().enabled = false;
-            Destroy(gameObject);
+            Destroy(gameObject,3.0f);
         }
         else 
         {
+            AudioManager.instance.Play("DragonDamage");
             animator.SetTrigger("damage");
         }
     }
