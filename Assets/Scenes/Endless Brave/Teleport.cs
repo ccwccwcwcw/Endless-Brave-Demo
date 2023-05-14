c0cca9498c1cef2969b56d83497edd246bf4baed
lilzjllx <zhuojia123@gmail.com>
2023-05-14 18:49:01 +0800

    new map
    teleport

---
+++ Assets/Scenes/Endless Brave/Teleport.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    
+
+    public Transform destination, player;
+    public GameObject playerg;
+
+    private void OnTriggerEnter(Collider other)
+    {
+                Debug.Log("Player collided with flower");
+
+        if (other.CompareTag("Player"))
+        {
+            playerg.SetActive(false);
+            player.position = destination.position;
+            playerg.SetActive(true);
+        }
+    }
+}
