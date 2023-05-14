e9bb7103d58ea1d60a84da23ad389d60251b7e1d
carfk <louis200001@gmail.com>
2023-05-15 03:32:11 +0800

    Hide ChatBubble

--- Assets/Script(Old)/ChatBubble.cs
+++ Assets/Script(Old)/ChatBubble.cs
@@ -7,16 +7,59 @@ public class ChatBubble : MonoBehaviour
     private SpriteRenderer backgroundSpriteRenderer;
     private SpriteRenderer iconSpriteRenderer;
     private TextMeshPro textMeshPro;
+    [SerializeField] private GameObject chatBubble;
+    private void Awake()
+    {
+        chatBubble.SetActive(false);
+        Transform backgroundTransform = transform.Find("Background");
+        if (backgroundTransform != null)
+        {
+            backgroundSpriteRenderer = backgroundTransform.GetComponent<SpriteRenderer>();
+        }
 
-    private void Awake() {
-        backgroundSpriteRenderer = transform.Find("Background").GetComponent<SpriteRenderer>();
-        iconSpriteRenderer = transform.Find("Background").GetComponent<SpriteRenderer>();
-        textMeshPro =transform.Find("Text").GetComponent<TextMeshPro>();
+        Transform iconTransform = transform.Find("Icon");
+        if (iconTransform != null)
+        {
+            iconSpriteRenderer = iconTransform.GetComponent<SpriteRenderer>();
+        }
+
+        Transform textTransform = transform.Find("Text");
+        if (textTransform != null)
+        {
+            textMeshPro = textTransform.GetComponent<TextMeshPro>();
+        }
     }
     private void Start() {
+        chatBubble.SetActive(false);
         Setup("pikachu");
     }
     private void Setup(string text) {
         textMeshPro.SetText(text);
     }
-}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            chatBubble.SetActive(true);
+            Debug.Log("Player entered the trigger!");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+       {
+            chatBubble.SetActive(false);
+            Debug.Log("Player exited the trigger!");
+       }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player in the trigger!");
+
+        }
+    }
+        }
