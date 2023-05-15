21a7a225f516f13eb214d9a6bcddbbc95a653ffd
ccwccwcwcw <ccwccwcwcw@gmail.com>
2023-05-15 11:06:38 +0800

    All scene added latest XR Parts
    Test walk with headset ok except forest

--- Assets/Endless Brave/Script/Slot.cs
+++ Assets/Endless Brave/Script/Slot.cs
@@ -26,18 +26,18 @@ public class Slot : MonoBehaviour
             InsertItem(obj);
         }
     }
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if (gameObject.GetComponent<Item>() == null) return;
-    //    if (gameObject.GetComponent<Item>().inSlot)
-    //    {
-    //        gameObject.GetComponentInParent<Slot>().ItemInSlot = null;
-    //        gameObject.transform.parent = null;
-    //        gameObject.GetComponent<Item>().inSlot = false;
-    //        gameObject.GetComponent<Item>().currentSlot.ResetColor();
-    //        gameObject.GetComponent<Item>().currentSlot = null;
-    //    }
-    //}
+    private void OnTriggerExit(Collider other)
+    {
+        if (gameObject.GetComponent<Item>() == null) return;
+        if (gameObject.GetComponent<Item>().inSlot)
+        {
+            gameObject.GetComponentInParent<Slot>().ItemInSlot = null;
+            gameObject.transform.parent = null;
+            gameObject.GetComponent<Item>().inSlot = false;
+            gameObject.GetComponent<Item>().currentSlot.ResetColor();
+            gameObject.GetComponent<Item>().currentSlot = null;
+        }
+    }
 
     bool IsItem(GameObject obj)
     {
