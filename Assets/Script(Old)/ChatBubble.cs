7a8269b303eeb871380018e55ccd90bcaccc1bbf
unknown <a1@tyit35626.vtc.hk>
2023-05-15 17:01:12 +0800

    final

--- Assets/Script(Old)/ChatBubble.cs
+++ Assets/Script(Old)/ChatBubble.cs
@@ -37,29 +37,6 @@ public class ChatBubble : MonoBehaviour
         textMeshPro.SetText(text);
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            chatBubble.SetActive(true);
-            Debug.Log("Player entered the trigger!");
-        }
-    }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-       {
-            chatBubble.SetActive(false);
-            Debug.Log("Player exited the trigger!");
-       }
-    }
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Player in the trigger!");
 
-        }
-    }
         }
